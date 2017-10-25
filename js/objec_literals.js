d02448b885d34ee12504c97186dcffd4c1f52835
Nastya513 <kelnmiira@mail.ru>
2017-10-25 18:33:41 +0300

    solution task#6  (Singleton).

--- js/objec_literals.js
+++ js/objec_literals.js
@@ -44,3 +44,23 @@ console.log(startNode("Identifier", "foo", {
 // → {type: "Identifier",
 //    value: "foo",
 //    src: "bar.js"}
+
+
+/*
+ ---------------------------------------------------------------------------------------------
+ */
+console.log('------------st task6');
+/*
+ Task#6 Singleton
+
+ Add a get method to the ids object, which returns the next id and increments its next counter. Use the short method syntax.
+ */
+
+var ids = {
+    next: 0,
+    get(){
+        return this.next++
+    }
+};
+console.log(ids.get());// → 0
+console.log(ids.get());// → 1
