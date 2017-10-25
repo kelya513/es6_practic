4f815e9164678f412e1096ea24a6bf7db5bc733f
Nastya513 <kelnmiira@mail.ru>
2017-10-25 16:53:56 +0300

    solution task#2(class inheritance)

--- js/classes.js
+++ js/classes.js
@@ -1,3 +1,4 @@
+console.log('------------st task1');
 /*
  Task#1 Point
 
@@ -25,3 +26,37 @@ class Point{
 
 console.log(new Point(1, 2).plus(new Point(2, 1))); // → Point{x: 3, y: 3}
 
+/*
+---------------------------------------------------------------------------------------------
+ */
+console.log('------------st task2');
+/*
+ Task#2 Speaker upgrade
+
+ Rewrite these two object types to use the class keyword, instead of direct prototype manipulation.
+ Speaker is a simple type that exposes a speak method which, when called, logs the given text along with the speaker's name.
+ Shouter is a subtype of Speaker which shouts its text and makes it uppercase.
+ */
+
+class Speaker{
+    constructor(name, verb="says"){
+        this.name = name;
+        this.verb = verb;
+    }
+
+    speak(text){
+        console.log(`${this.name} ${this.verb} '${text}'`);
+    }
+}
+
+class Shouter extends Speaker{
+    constructor(name){
+        super(name, "shouts");
+    }
+
+    speak(text){
+        super.speak(text.toUpperCase());
+    }
+}
+
+new Shouter("Dr. Loudmouth").speak("hello there"); //Dr. Loudmouth shouts 'HELLO THERE'
