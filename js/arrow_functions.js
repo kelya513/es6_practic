7a30046614a96605f988ebdc52448a0cb3a1ff8a
Nastya513 <kelnmiira@mail.ru>
2017-10-26 10:59:15 +0300

    solution task#12 (Precedence).

--- js/arrow_functions.js
+++ js/arrow_functions.js
@@ -67,3 +67,23 @@ console.log("array:", sorted.content);
 /*
  ---------------------------------------------------------------------------------------------
  */
+console.log('------------st task12');
+/*
+ Task#11 Precedence
+
+ Where does an arrow function end? At a closing bracket or semicolon, of course.
+ But does a comma denote the end? Is the body of the function in this example x + y, 0, or just x + y?
+
+ Is there anything else that will end an arrow function body? Experiment.
+  */
+
+console.log([1, 2, 3].reduce((x, y) => x + y, 0)); //  (x, y) => x + y - function , 0 - параметр функции reduce
+console.log([1, 2, 3].reduce((x, y) => {return x + y}, 0)); // (x, y) => {return x + y} - function - {}
+console.log([1, 2, 3].reduce((x, y) => (x + y), 0)); // (x, y) => (x + y) - function  - ()
+console.log([1, 2, 3].reduce((x, y) => {
+                                        x += y;
+                                        return x;
+                                       }, 0)); // в несколько рядов
+/*
+ ---------------------------------------------------------------------------------------------
+ */
