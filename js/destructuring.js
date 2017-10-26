354e5d5ca25ea5bca5f279be33154eb50441921a
Nastya513 <kelnmiira@mail.ru>
2017-10-26 12:41:08 +0300

    fix task#13  solution task#14 (Default argument).

--- js/destructuring.js
+++ js/destructuring.js
@@ -19,8 +19,8 @@ console.log('------------st task13');
 //         "frobnifier:", frobnifier)
 // }
 
-function go(options) {
-    options = options ? options: {};//присвоение опций, если ничего не передали, то пустой объект, что бы вывести значение выставленные по умолчанию.
+function go(options={}) { // параметр по умолчанию
+  //  options = options ? options: {};//присвоение опций, если ничего не передали, то пустой объект, что бы вывести значение выставленные по умолчанию.
     let {
         speed = 4,
         enable: {
@@ -42,7 +42,28 @@ go({speed: 5});
 go({speed: 5, enable:{}});
 go();// вызов вообще без параметров не вызовет ошибку, и выведет все значения по умолчанию
 
+/*
+ ---------------------------------------------------------------------------------------------
+ */
+console.log('------------st task14');
+/*
+ Task#14 Default argument
 
+ It would be nice to be able to call our `lastIndexOf` function without providing the third argument,
+ and have it default to starting at the end of the array.
+ It would be even nicer if we could express this with an ES6 default argument value. Can we?
+
+ In which scope are default arguments evaluated? (Experiment to find out.)
+ */
+
+function lastIndexOf(arr=[], elt=0, start = arr.length) { // значение параметра по умолчанию, вычисляются при вызове функции
+    for (let i = start - 1; i >= 0; i--){
+        if (arr[i] === elt) return i;
+    }
+    return -1
+}
+
+console.log(lastIndexOf([1, 2, 1, 2], 2));
 
 /*
  ---------------------------------------------------------------------------------------------
