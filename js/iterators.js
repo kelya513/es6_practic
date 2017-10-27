a3fc0272ff3c5182c83382d9ed1ad990ddc4b55a
Nastya513 <kelnmiira@mail.ru>
2017-10-27 18:37:58 +0300

    solution task#26  (List.from).

--- js/iterators.js
+++ js/iterators.js
@@ -95,3 +95,66 @@ for (let elt of take(3, integers()))
 /*
  ---------------------------------------------------------------------------------------------
  */
+console.log('------------st task27');
+/*
+ Task#26 List.from
+
+ Here is our List class again. In ES6, Array has a static method from, which does the thing that you'd
+ now use Array.prototype.slice.call for—turn an array-like object (like arguments or node.childNodes)
+ into a real array. If its argument it iterable, it'll take the values from the iterator, and create an
+ array from that. If not, it'll look for a length property, and use integer indexes to fetch element values.
+
+ Add a static from method to the List class, which behaves the same way: It takes a single argument.
+ If that argument is iterable (has a Symbol.iterator property), it iterates of it, and creates a list from its content.
+ If not, it loops over the object's indices (using .length), and builds a list from its content.
+
+ Note that lists are easiest to build from back to front.
+ You might want to first convert an iterable to an array to make building your list easier.
+ Alternatively, use mutation to build the list front-to-back.
+ */
+
+class List2 {
+    constructor(head, tail) {
+        this.head = head;
+        this.tail = tail;
+    }
+
+    map(f){
+        return new List2(f(this.head), this.tail && this.tail.map(f))
+    }
+
+    static isIterable(obj) {
+        // checks for null and undefined
+        if (obj == null) {
+            return false;
+        }
+        return typeof obj[Symbol.iterator] === 'function';
+    }
+
+    static fromM(obj){
+
+        let newObj, arr;
+        if (this.isIterable(obj)){
+            arr = Array.isArray(obj) ? obj : obj.split('');
+            arr.reverse();
+            newObj = new List2(arr[0], null);
+            for(let i = 1; i < arr.length; i++){
+                newObj = new List2(arr[i], newObj);
+            }
+            return newObj;
+        }
+
+        return 'не итерируется';
+    }
+
+}
+
+
+console.log(List2.fromM([3, 2, 1]));// → List{head: 3, tail: List{head: 2, tail: List{head: 1, tail: null}}}
+console.log(List2.fromM(5485)); // 'не итерируется';
+console.log(List2.fromM('sdsad')); //→ List{head:s, tail: ....
+
+
+/*
+ ---------------------------------------------------------------------------------------------
+ */
