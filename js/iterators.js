79f49550c6dbf45817165080b909217282787050
Nastya513 <kelnmiira@mail.ru>
2017-10-27 16:20:59 +0300

    solution task#25  (Take N).

--- js/iterators.js
+++ js/iterators.js
@@ -54,3 +54,44 @@ for (let elt of lists) console.log(elt)
 /*
  ---------------------------------------------------------------------------------------------
  */
+
+console.log('------------st task26');
+/*
+ Task#26 Take N
+
+ The integers function creates an infinite iterator, that keeps on producing integers forever.
+
+ Implement the take function, which wraps an iterator in another iterator, but cutting it off after n elements.
+ */
+
+
+function integers() {
+    let i = 0;
+    return {next() { return {value: i++} },
+        [Symbol.iterator]() { return this }}
+}
+
+function take(n, iter) {
+    return {
+        next() {
+            let next = iter.next().value;
+
+            if ( next < n){
+                return {done:false, value: next}
+            }
+
+            return {done:true}
+        },
+        [Symbol.iterator]() { return this }
+    }
+}
+
+for (let elt of take(3, integers()))
+    console.log(elt)
+// → 0
+//   1
+//   2
+
+/*
+ ---------------------------------------------------------------------------------------------
+ */
